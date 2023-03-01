6082e737686f8d63be17849ab6f94348f631c7bd
drebezovan <drebezovan@mail.ru>
2023-03-01 09:43:12 +0300

    Added System.in

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new FileReader("src/main/java/org/example/input.txt"));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Как тебя зовут?");
         String name = sc.next();
         System.out.println("Здравствуй, " + name);
